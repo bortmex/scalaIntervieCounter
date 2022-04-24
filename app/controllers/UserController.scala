2cf867345c938dea4a81589cb32a5f73fe577a2c
Alex <alexanderogov@gmail.com>
2022-04-25 01:38:00 +0300

    Delete path for users Heroku

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -8,8 +8,6 @@ import play.api.mvc._
 import services.UserService
 
 import javax.inject._
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class UserController @Inject()(
                                 cc: MessagesControllerComponents,
@@ -18,49 +16,49 @@ class UserController @Inject()(
 
   implicit val userFormat = Json.format[User]
 
-  def getAll = Action.async { implicit request: Request[AnyContent] =>
-    userService.listAllItems map { items =>
-      Ok(Json.toJson(items))
-    }
-  }
-
-  def getById(id: Long) = Action.async { implicit request: Request[AnyContent] =>
-    userService.getItem(id) map { item =>
-      Ok(Json.toJson(item))
-    }
-  }
-
-  def add = Action.async { implicit request: Request[AnyContent] =>
-    UserForm.form.bindFromRequest.fold(
-      // if any error in submitted data
-      errorForm => {
-        errorForm.errors.foreach(println)
-        Future.successful(BadRequest("Error!"))
-      },
-      data => {
-        val newTodoItem = User(0, data.name, data.password)
-        userService.addItem(newTodoItem).map(_ => Redirect(routes.UserController.getAll))
-      })
-  }
-
-  def update(id: Long) = Action.async { implicit request: Request[AnyContent] =>
-    UserForm.form.bindFromRequest.fold(
-      // if any error in submitted data
-      errorForm => {
-        errorForm.errors.foreach(println)
-        Future.successful(BadRequest("Error!"))
-      },
-      data => {
-        val todoItem = User(id, data.name, data.password)
-        userService.updateItem(todoItem).map(_ => Redirect(routes.UserController.getAll))
-      })
-  }
-
-  def delete(id: Long) = Action.async { implicit request: Request[AnyContent] =>
-    userService.deleteItem(id) map { res =>
-      Redirect(routes.UserController.getAll)
-    }
-  }
+//  def getAll = Action.async { implicit request: Request[AnyContent] =>
+//    userService.listAllItems map { items =>
+//      Ok(Json.toJson(items))
+//    }
+//  }
+//
+//  def getById(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+//    userService.getItem(id) map { item =>
+//      Ok(Json.toJson(item))
+//    }
+//  }
+//
+//  def add = Action.async { implicit request: Request[AnyContent] =>
+//    UserForm.form.bindFromRequest.fold(
+//      // if any error in submitted data
+//      errorForm => {
+//        errorForm.errors.foreach(println)
+//        Future.successful(BadRequest("Error!"))
+//      },
+//      data => {
+//        val newTodoItem = User(0, data.name, data.password)
+//        userService.addItem(newTodoItem).map(_ => Redirect(routes.UserController.getAll))
+//      })
+//  }
+//
+//  def update(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+//    UserForm.form.bindFromRequest.fold(
+//      // if any error in submitted data
+//      errorForm => {
+//        errorForm.errors.foreach(println)
+//        Future.successful(BadRequest("Error!"))
+//      },
+//      data => {
+//        val todoItem = User(id, data.name, data.password)
+//        userService.updateItem(todoItem).map(_ => Redirect(routes.UserController.getAll))
+//      })
+//  }
+//
+//  def delete(id: Long) = Action.async { implicit request: Request[AnyContent] =>
+//    userService.deleteItem(id) map { res =>
+//      Redirect(routes.UserController.getAll)
+//    }
+//  }
 
   def processLoginAttempt = Action { implicit request: MessagesRequest[AnyContent] =>
     val errorFunction = { formWithErrors: Form[UserFormData] =>
